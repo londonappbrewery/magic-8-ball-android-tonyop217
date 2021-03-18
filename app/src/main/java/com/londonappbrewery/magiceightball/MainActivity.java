6fdbb6bd1f1324371f2fae058322a1103c3238e6
tonyop217 <tony_xbox360rules@hotmail.com>
2021-03-18 16:40:42 -0100

    Made my eightball app :)

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button rollButton = (Button)findViewById(R.id.button2);
+
+        final ImageView eightBall = (ImageView)findViewById(R.id.ball_image);
+
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("EightBall", "Ze button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(5);
+
+                eightBall.setImageResource(ballArray[number]);
+            }
+        });
     }
+
+
 }
